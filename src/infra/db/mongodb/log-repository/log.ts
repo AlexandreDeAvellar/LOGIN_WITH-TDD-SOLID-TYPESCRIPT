5293d76d87a2f7131b476ae61bbc263f2171882d
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-11-10 13:00:35 -0300

    feat: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,13 @@
+import LogErrorRepositoryProtocol from '../../../../data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LogMongoRepository implements LogErrorRepositoryProtocol {
+  async logError (stack: string): Promise<void> {
+    const collectionError = await MongoHelper.getCollection('errors')
+    await collectionError.insertOne({
+      stack,
+      date: new Date()
+    })
+    return await new Promise(resolve => resolve())
+  }
+}
