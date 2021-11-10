a5b171c8e80e7de83d46c2dfce1b297f7a8336cb
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-11-09 23:17:03 -0300

    refactor: change method name on LogErrorRepositoryProtocol

--- src/data/protocols/log-error-repository.ts
+++ src/data/protocols/log-error-repository.ts
@@ -1,3 +1,3 @@
 export default interface LogErrorRepositoryProtocol {
-  log: (stack: string) => Promise<void>
+  logError: (stack: string) => Promise<void>
 }
--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -8,7 +8,7 @@ export class LogControllerDecorator implements Controller {
     const httpResponse = await this.controller.handle(httpRequest)
     if (httpResponse.statusCode === 500) {
       const stackError = httpResponse.body.stack
-      await this.logErrorRepositopry.log(stackError)
+      await this.logErrorRepositopry.logError(stackError)
       return httpResponse
     }
     return httpResponse
