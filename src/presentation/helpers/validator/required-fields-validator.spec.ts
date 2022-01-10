af577dd78679fbc0b0a115889a50167dcfdb9e32
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-01-10 20:59:37 -0300

    test: ensure RequiredFields Validator returns MissingParamError if any field is missing and returns null if field is informed

---
+++ src/presentation/helpers/validator/required-fields-validator.spec.ts
@@ -0,0 +1,20 @@
+import { RequiredFieldsValidator } from '.'
+import { MissingParamError } from '../../errors'
+
+const makeSut = (): RequiredFieldsValidator => {
+  return new RequiredFieldsValidator('any_field')
+}
+
+describe('RequiredField Validator', () => {
+  it('should return MissingParam if field is missing', () => {
+    const sut = makeSut()
+    const error = sut.validate({})
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+
+  it('should return null if field is informed', () => {
+    const sut = makeSut()
+    const error = sut.validate({ any_field: 'any_value' })
+    expect(error).toBe(null)
+  })
+})
