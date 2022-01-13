566d1d496508852ef321b92be48fc0f3281a246b
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-01-12 22:34:28 -0300

    refactor: move validators to ValidatorComposite

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,22 +1,20 @@
-import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from './login-protocol'
+import { Validator } from '../signup/signup-protocol'
+import { Controller, HttpRequest, HttpResponse, Authentication } from './login-protocol'
 
 export class LoginController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator,
+    private readonly validator: Validator,
     private readonly authentication: Authentication
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['email', 'password']
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      const error = this.validator.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
       }
       const { email, password } = httpRequest.body
-      const isValid = this.emailValidator.isValid(email)
-      if (!isValid) return badRequest(new InvalidParamError('email'))
       const accessToken = await this.authentication.auth(email, password)
       if (!accessToken) {
         return unauthorized()
