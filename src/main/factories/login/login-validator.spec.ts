0e286a783ef60d72075584d29d986dd15ee994ec
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-01-13 20:47:04 -0300

    testt: ensure LoginValidator call ValidationComposite with all validations

---
+++ src/main/factories/login/login-validator.spec.ts
@@ -0,0 +1,26 @@
+import { EmailValidation, RequiredFieldsValidator, Validator, ValidatorComposite } from '../../../presentation/helpers/validator'
+import { EmailValidator } from '../../../presentation/protocols/email-validator'
+import { makeLoginValidator } from './login-validator'
+
+jest.mock('../../../presentation/helpers/validator/validator-composite')
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): Boolean {
+      return true
+    };
+  }
+  return new EmailValidatorStub()
+}
+
+describe('LoginValidator Factory', () => {
+  test('should call ValidationComposite with all validations', () => {
+    makeLoginValidator()
+    const validations: Validator[] = []
+    for (const validator of ['email', 'password']) {
+      validations.push(new RequiredFieldsValidator(validator))
+    }
+    validations.push(new EmailValidation('email', makeEmailValidator()))
+    expect(ValidatorComposite).toHaveBeenCalledWith(validations)
+  })
+})
