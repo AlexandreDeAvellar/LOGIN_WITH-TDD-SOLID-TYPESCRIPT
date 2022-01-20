c30d0ebdbd428fabd6de8547917be0cf5fb4dfb1
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-01-19 21:09:25 -0300

    refactor: move repositories protocols to db folder

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,4 +1,4 @@
-import LogErrorRepositoryProtocol from '../../data/protocols/log-error-repository'
+import LogErrorRepositoryProtocol from '../../data/protocols/db/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 import { ok, serverError } from '../../presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
