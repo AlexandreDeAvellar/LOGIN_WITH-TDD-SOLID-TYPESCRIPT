fadbd9b2b4d1580aa6a7170064fa667fa5c6cebd
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-04-07 21:49:46 -0300

    feat: created factory to DbADdAccount

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,19 +1,13 @@
-import { DbAddAccount } from '../../../../data/usecase/add-account/db-add-account'
-import { BcryptAdaper } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { AccountMongoRepository } from '../../../../infra/db/mongodb/account-repository/account-mongo-repository'
 import { LogMongoRepository } from '../../../../infra/db/mongodb/log-repository/log-mongo-repository'
 import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
 import { Controller } from '../../../../presentation/protocols'
 import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
+import { makeDbAddAccountFactory } from '../../usecases/add-account/db-add-account-factory'
 import { makeAuthenticationFactory } from '../../usecases/authentication/db-authentication-factory'
 import { makeSignupValidator } from './signup-validator'
 
 export const makeSignUpController = (): Controller => {
-  const salt = 12
-  const addAccountRepository = new AccountMongoRepository()
-  const bcrypt = new BcryptAdaper(salt)
-  const addAccount = new DbAddAccount(bcrypt, addAccountRepository)
-  const signUpController = new SignUpController(addAccount, makeSignupValidator(), makeAuthenticationFactory())
+  const signUpController = new SignUpController(makeDbAddAccountFactory(), makeSignupValidator(), makeAuthenticationFactory())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
---
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -0,0 +1,11 @@
+import { AddAccount } from '../../../../domain/usecases/add-account'
+import { DbAddAccount } from '../../../../data/usecase/add-account/db-add-account'
+import { BcryptAdaper } from '../../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account-repository/account-mongo-repository'
+
+export const makeDbAddAccountFactory = (): AddAccount => {
+  const salt = 12
+  const addAccountRepository = new AccountMongoRepository()
+  const bcrypt = new BcryptAdaper(salt)
+  return new DbAddAccount(bcrypt, addAccountRepository)
+}
