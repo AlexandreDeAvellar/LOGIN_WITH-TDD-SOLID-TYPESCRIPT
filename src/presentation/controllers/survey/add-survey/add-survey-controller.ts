4153cc63f5a88632211c64125b93405bc63e7e4e
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-04-13 20:54:30 -0300

    feat: ensure AddSurveyController return 500 if AddSurvey throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest, Controller, HttpRequest, HttpResponse, AddSurvey, Validator } from './add-survey-protocol'
+import { badRequest, Controller, HttpRequest, HttpResponse, AddSurvey, Validator, serverError } from './add-survey-protocol'
 
 export class AddSurveyController implements Controller {
   constructor (
@@ -7,17 +7,21 @@ export class AddSurveyController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validateResponse = this.validator.validate(httpRequest.body)
-    if (validateResponse) return badRequest(validateResponse)
-    const { question, answers } = httpRequest.body
-    await this.survey.add({
-      question,
-      answers
-    })
-    const httpResponse: HttpResponse = {
-      statusCode: 200,
-      body: ''
+    try {
+      const validateResponse = this.validator.validate(httpRequest.body)
+      if (validateResponse) return badRequest(validateResponse)
+      const { question, answers } = httpRequest.body
+      await this.survey.add({
+        question,
+        answers
+      })
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: ''
+      }
+      return await new Promise(resolve => resolve(httpResponse))
+    } catch (error) {
+      return serverError(error)
     }
-    return await new Promise(resolve => resolve(httpResponse))
   }
 }
