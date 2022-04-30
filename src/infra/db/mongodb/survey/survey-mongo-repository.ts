58ec49d5cc14ec5e8f5051a9c7da61a97b3417a6
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-04-30 10:45:53 -0300

    feat: ensure SurveyMongoRepository add survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,9 @@
+
+import { AddSurveyModel, AddSurveyRepository, MongoHelper } from './survey-mongo-repository-protocols'
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+  async add (surveyData: AddSurveyModel): Promise<void> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.insertOne(surveyData)
+  }
+}
