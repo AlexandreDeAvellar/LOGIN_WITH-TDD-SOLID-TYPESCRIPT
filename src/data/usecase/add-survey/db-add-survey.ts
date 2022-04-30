08d25e5ea8d8a0e604678ed4564e9ec7e35cec6c
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-04-29 21:54:21 -0300

    feat: ensure  DbAddSurvey Usecase throw if AddSurveyRepository throws

--- src/data/usecase/add-survey/db-add-survey.ts
+++ src/data/usecase/add-survey/db-add-survey.ts
@@ -3,6 +3,10 @@ import { AddSurvey, AddSurveyModel, AddSurveyRepository } from './db-add-survey-
 export class DbAddSurvey implements AddSurvey {
   constructor (private readonly addSurveyRepository: AddSurveyRepository) {}
   async add (data: AddSurveyModel): Promise<void> {
-    await this.addSurveyRepository.add(data)
+    try {
+      await this.addSurveyRepository.add(data)
+    } catch (error) {
+      throw new Error()
+    }
   }
 }
