84be7186043137078cdb390991b1cfd306319e65
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-05-07 11:10:27 -0300

    test: ensure AccountMongoRepository returns an account on loadByToken without role

--- src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account-repository/account-mongo-repository.spec.ts
@@ -74,4 +74,17 @@ describe('Account Mongo Repository', () => {
       expect(account?.accessToken).toBe('any_token')
     })
   })
+
+  describe('loadBytoken()', () => {
+    test('should return an account on loadByToken without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({ ...fakeAccountModel, accessToken: 'any_token' })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toBe('any_name')
+      expect(account?.email).toBe('any_email@mail.com')
+      expect(account?.password).toBe('any_password')
+    })
+  })
 })
