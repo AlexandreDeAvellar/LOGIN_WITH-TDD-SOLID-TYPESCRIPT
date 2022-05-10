fb5c00c4ed03a9a72d6adf9394aa00e97bfff16d
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-05-09 21:28:19 -0300

    feat: ensure SurveyRoutes return 403 on add survey without accessToken

---
+++ src/main/factories/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -0,0 +1,11 @@
+import { DbLoadAccountByToken } from '../../../../data/usecase/load-account-by-token/db-load-account-by-token'
+import { LoadAccountByToken } from '../../../../domain/usecases/load-account-by-token'
+import { JwtAdapter } from '../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../infra/db/mongodb/account-repository/account-mongo-repository'
+import env from '../../../config/env'
+
+export const makeDbLoadAccountByToken = (): LoadAccountByToken => {
+  const decrypt = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbLoadAccountByToken(decrypt, accountMongoRepository)
+}
