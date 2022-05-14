32a51e49fdeea70de0d0e06ec2a9e6e4b8809ea1
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-05-13 22:45:13 -0300

    refactor: move surveys content to a folder

--- src/data/usecase/add-survey/db-add-survey-protocol.ts
+++ src/data/usecase/add-survey/db-add-survey-protocol.ts
@@ -1,3 +1,3 @@
 export * from '../../../domain/models/surveys'
 export * from '../../../domain/usecases/add-survey'
-export * from '../../protocols/db/add-survey/add-survey-repository'
+export * from '../../protocols/db/survey/add-survey/add-survey-repository'
