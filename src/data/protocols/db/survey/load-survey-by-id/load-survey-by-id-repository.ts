f9a71fa33e3c4eeb302d73fda8f78771e4e0c64a
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-05-31 21:05:51 -0300

    feat: emsure DbLoadSurveyById calls LoadSurveyByIdRepository with correct value

---
+++ src/data/protocols/db/survey/load-survey-by-id/load-survey-by-id-repository.ts
@@ -0,0 +1,7 @@
+import { SurveyModel } from '../../../../../domain/models/surveys'
+
+export interface SurveyModelRepo extends SurveyModel { }
+
+export interface LoadSurveyByIdRepository {
+  loadById: (id: string) => Promise<SurveyModelRepo>
+}
---
+++ src/data/usecase/load-survey-by-id/load-survey-by-id-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../protocols/db/survey/load-survey-by-id/load-survey-by-id-repository'
+export * from '../../../domain/usecases/load-survey-by-id'
+export * from '../../../domain/models/surveys'
