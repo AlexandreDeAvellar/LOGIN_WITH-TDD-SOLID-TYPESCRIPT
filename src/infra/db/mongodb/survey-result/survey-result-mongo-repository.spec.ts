7e101ea1e51a4ce2a20860d576ba963c109012a6
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-06-02 20:58:05 -0300

    test: ensure SurveyResultMongoRepository add a survey result if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,44 @@
+import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
+import { SurveyResultDataRepo, MongoHelper } from './survey-result-mongo-repository-protocols'
+import { Collection } from 'mongodb'
+import MockDate from 'mockdate'
+
+const makeFakeSurveyResultDataRepo = (): SurveyResultDataRepo => ({
+  accountId: 'any_id', answer: 'any_answer', date: new Date(), surveyId: 'any_id'
+})
+
+interface SutTypes {
+  sut: SurveyResultMongoRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new SurveyResultMongoRepository()
+  return { sut }
+}
+
+let surveyResult: Collection
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL as string)
+  MockDate.set(new Date())
+})
+
+beforeEach(async () => {
+  surveyResult = await MongoHelper.getCollection('surveyResults')
+  await surveyResult.deleteMany({})
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+  MockDate.reset()
+})
+
+describe('SurveyResultMongoRepository', () => {
+  describe('save()', () => {
+    test('should add a survey result if its new', async () => {
+      const { sut } = makeSut()
+      const surveyResultModel = await sut.save(makeFakeSurveyResultDataRepo())
+      expect(surveyResultModel.id).toBeTruthy()
+      expect(surveyResultModel).toEqual(expect.objectContaining(makeFakeSurveyResultDataRepo()))
+    })
+  })
+})
