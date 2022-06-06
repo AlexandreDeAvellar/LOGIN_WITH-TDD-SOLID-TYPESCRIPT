2fb072d71401dbe386402f979ab8bd88b88a12ba
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-06-06 12:47:19 -0300

    feat: ensure SaveSurveyResultController return 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,11 +1,15 @@
-import { LoadSurveyById, Controller, HttpRequest, HttpResponse, forbidden, InvalidParamError } from './save-survey-result-controller-protocols'
+import { LoadSurveyById, Controller, HttpRequest, HttpResponse, forbidden, InvalidParamError, serverError } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyByIdRepository: LoadSurveyById) {}
+  constructor (private readonly loadSurveyByIdRepository: LoadSurveyById) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { params: { id } } = httpRequest
-    const survey = await this.loadSurveyByIdRepository.loadById(id)
-    if (!survey) return forbidden(new InvalidParamError('surveyId'))
-    return await new Promise(resolve => resolve({ statusCode: 200, body: null }))
+    try {
+      const { params: { id } } = httpRequest
+      const survey = await this.loadSurveyByIdRepository.loadById(id)
+      if (!survey) return forbidden(new InvalidParamError('surveyId'))
+      return await new Promise(resolve => resolve({ statusCode: 200, body: null }))
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
