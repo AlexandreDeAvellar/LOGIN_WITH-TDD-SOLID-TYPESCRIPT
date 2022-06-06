d8ae37a67ae476811c0503674db1bda0afe446f2
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-06-06 20:29:51 -0300

    feat: ensure SaveSurveyResultController return 403 if no accounId are found

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,16 +1,21 @@
 import { LoadSurveyById, Controller, HttpRequest, HttpResponse, forbidden, InvalidParamError, serverError } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyByIdRepository: LoadSurveyById) { }
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById
+  ) { }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const { params: { id }, body: { answer } } = httpRequest
+      const { params: { id }, body: { answer }, accountId } = httpRequest
 
-      const survey = await this.loadSurveyByIdRepository.loadById(id)
+      const survey = await this.loadSurveyById.loadById(id)
       if (!survey) return forbidden(new InvalidParamError('surveyId'))
 
       const answers = survey.answers.map(answer => answer.answer)
       if (!answers.includes(answer)) return forbidden(new InvalidParamError('answer'))
+
+      if (!accountId) return forbidden(new InvalidParamError('accoundId'))
 
       return await new Promise(resolve => resolve({ statusCode: 200, body: answer }))
     } catch (error) {
