33f751d8ecf45b0b379ccee556477bc2fadef3ad
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-07-13 13:31:39 -0300

    docs: create login path

---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,17 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'Rota de login',
+    requestBody: {
+      content: { 'application/json': { schema: { $ref: '#/schemas/loginParams' } } }
+    },
+    responses: {
+      200: {
+        description: 'Retorna um access token em caso de sucesso',
+        content: {
+          'application/json': { schema: { $ref: '#/schemas/account' } }
+        }
+      }
+    }
+  }
+}
