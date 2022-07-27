f115bae4346295892d9ca50ee4043a5b9a8db1a6
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-07-27 13:18:44 -0300

    feat: create surveyResultModel and SurveyResultAnswer interfaces

--- src/domain/models/survey-result.ts
+++ src/domain/models/survey-result.ts
@@ -5,6 +5,16 @@ export interface SurveyResultData {
   date: Date
 }
 
-export interface SurveyResultModel extends SurveyResultData {
-  id: string
+export interface SurveyResultModel {
+  surveyId: string
+  question: string
+  answers: SurveyResultAnswerModel[]
+  date: Date
+}
+
+export interface SurveyResultAnswerModel {
+  image?: string
+  answer: string
+  count: number
+  percent: number
 }
