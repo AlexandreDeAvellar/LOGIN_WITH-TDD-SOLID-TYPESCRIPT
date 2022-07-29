886763335fb1df2e9379efac32e88624b68adb28
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-07-29 09:11:53 -0300

    feat: ensure DbLoadSurveyResult return SurveyResultModel on success

--- src/data/usecase/load-survey-result/db-load-survey-result.ts
+++ src/data/usecase/load-survey-result/db-load-survey-result.ts
@@ -6,7 +6,7 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
   ) {}
 
   async load (surveyId: string): Promise<SurveyResultModel> {
-    await this.loadSurveyResultRepository.load(surveyId)
-    return { answers: [], date: new Date(), question: '', surveyId: '' }
+    const surveyResult = await this.loadSurveyResultRepository.load(surveyId)
+    return surveyResult
   }
 }
