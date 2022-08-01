452c66121603309818e84ecc7b9c06665d24ab25
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-08-01 20:36:00 -0300

    feat: create route GET /surveys/:surveyId/results

--- src/main/routes/survey-result.ts
+++ src/main/routes/survey-result.ts
@@ -3,7 +3,9 @@ import { Router } from 'express'
 import { auth } from '../middlewares/auth'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeSaveSurveyResultController } from '../factories/controllers/survey-result/save-survey-result-controller-factory'
+import { makeLoadSurveyResultController } from '../factories/controllers/survey-result/load-survey-result-controller/load-survey-result-controller-factory'
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+  router.get('/surveys/:surveyId/results', auth, adaptRoute(makeLoadSurveyResultController()))
 }
