d7a2cfe8abdc0d8e7a5f13310b63f3673fd2ec33
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2022-08-01 13:36:43 -0300

    test: ensure LoadSurveyResultController return 200 on success

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controler.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controler.spec.ts
@@ -1,8 +1,10 @@
 import { LoadSurveyResultController } from './load-survey-result-controler'
-import { LoadSurveyResultRepository, serverError, forbidden, InvalidParamError, LoadSurveyByIdRepository } from './load-survey-result-controler-protocols'
+import { LoadSurveyResultRepository, serverError, forbidden, InvalidParamError, LoadSurveyByIdRepository, ok } from './load-survey-result-controler-protocols'
 import { HttpRequest } from '../../../protocols'
 import { makeLoadSurveyResultRepositoryStub } from '../../../../data/usecase/load-survey-result/db-load-survey-result-mocks'
 import { makeLoadSurveyByIdRepositoryStub } from '../../../../data/usecase/load-survey-by-id/db-load-survey-by-id-mocks'
+import { makeFakeSurveyResultModelRepo } from '../../../../data/usecase/save-survey-result/db-save-survey-result-mocks'
+import MockDate from 'mockdate'
 
 const makeFakeHttpRequest = (): HttpRequest => ({ params: { surveyId: 'any_surveyId' } })
 
@@ -20,6 +22,9 @@ const makeSut = (): SutTypes => {
 }
 
 describe('LoadSurveyResultController', () => {
+  beforeAll(() => MockDate.set(new Date()))
+  afterAll(() => MockDate.reset())
+
   test('should call LoadSurveyById with correct values', async () => {
     const { sut, loadSurveyByIdStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
@@ -53,5 +58,11 @@ describe('LoadSurveyResultController', () => {
     jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockReturnValueOnce(Promise.reject(new Error()))
     const httpResponse = await sut.handle(makeFakeHttpRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+    expect(httpResponse).toEqual(ok(makeFakeSurveyResultModelRepo()))
   })
 })
